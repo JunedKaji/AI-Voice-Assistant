38ac1c01d5a4435f36f31b2e6cf62212e674417f
JunedKaji <kazijuned131@gmail.com>
2025-05-22 18:25:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import eel
+from engine.command import *
+from engine.features import *
+from engine.auth import recognize
+
+def start():
+    eel.init('www')
+    # flag = recognize.AuthenticateFace()
+    os.system('start chrome.exe --app="http://localhost:8000/index.html"')
+    eel.start('index.html', mode=None, host='localhost', block=True)
